8c3150fb3d6d786173b80ddbd3f9f804996e0942
Aron Friedman <aronfriedman98@gmail.com>
2022-05-25 23:35:40 -0400

    solution008 attempt

---
+++ js/solution008.js
@@ -0,0 +1,10 @@
+// input hour, minute, seconds, and calculate the total amount of mileseconds 
+// P: 3 positive ints
+// R: positive int
+
+
+
+
+const past = (h,m,s) => console.log(s*100 + m*100*60 + h*100*60*60)
+
+past(1,1,1)  // 366100
