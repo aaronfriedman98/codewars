be56e7a512a51a071257d1f73f70bc34a60eaa02
Aron Friedman <aronfriedman98@gmail.com>
2022-05-29 01:18:07 -0400

    solution012 completed

--- js/solution012.js
+++ js/solution012.js
@@ -1,1 +1,21 @@
-//
+// test if 2 strings are anagrams
+// P: 2 strings
+// R: true or false
+
+function isAnagram(string1, string2){
+    let array1 = string1.split('')
+    let array2 = string2.split('')
+    array1.sort()
+    array2.sort()
+    let newString1 = array1.join('')
+    let newString2 = array2.join('')
+    if(newString1 === newString2){
+        return true
+    }
+    else{
+        return false
+    }
+}
+
+console.log(isAnagram("coffee", "feefoc")) // true
+console.log(isAnagram("coffee", "fofee")) // false
