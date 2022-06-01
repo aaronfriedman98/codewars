93d846d88c1b476fb5c7d67b56c5481c04bf61ed
Aron Friedman <aronfriedman98@gmail.com>
2022-05-31 23:24:54 -0400

    solution017

---
+++ js/solution017.js
@@ -0,0 +1,10 @@
+// pass in an array with integer pairs, the first int determines how many ppl get on the bus, the 2nd int determines how many people get off
+// display the total number of ppl remaining after the array
+// P: array
+// R: int, not negative
+
+function number(array){
+
+}
+
+console.log(number([]))
