5982f4035605fc4757cf8af2ad504e9cf154ab6a
Aron Friedman <aronfriedman98@gmail.com>
2022-06-02 02:46:02 -0400

    solution018 completed

--- js/solution018.js
+++ js/solution018.js
@@ -1,3 +1,8 @@
-//
-// P:
-// R: 
+// find the Nth power of the int which is in the index of N in the array
+// P: array with positive numbers, positive int
+// R: positive int or -1
+
+const index = (array, n) => array.length > n ? array[n]*array[n] : -1
+
+console.log(index([3,2,4], 2)) // 16
+console.log(index([2,4,2], 3)) // -1
