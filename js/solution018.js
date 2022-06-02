f004a4e36518420a58b6c930ea812455f7e50636
Aron Friedman <aronfriedman98@gmail.com>
2022-06-02 02:52:02 -0400

    solution018 completed

--- js/solution018.js
+++ js/solution018.js
@@ -2,7 +2,7 @@
 // P: array with positive numbers, positive int
 // R: positive int or -1
 
-const index = (array, n) => array.length > n ? array[n]*array[n] : -1
+const index = (array, n) => array.length > n ? array[n]**n : -1
 
 console.log(index([3,2,4], 2)) // 16
 console.log(index([2,4,2], 3)) // -1
