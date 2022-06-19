30d290e974399a804a46d5b4d6cee487d8c838ed
Aron Friedman <aronfriedman98@gmail.com>
2022-06-19 03:24:43 -0400

    solution028 completed

--- js/solution028.js
+++ js/solution028.js
@@ -1,1 +1,9 @@
-//
+// return a string - "x years old" with x being a random number between 1 - 9
+// R: string
+
+const getAge = () => Math.floor(Math.random() * 10) * 1 + " years old"
+
+
+console.log(getAge())
+console.log(getAge())
+console.log(getAge())
