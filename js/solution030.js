a5e1e5f4cef675779322b79ccbfd5f8442c400c8
Aron Friedman <aronfriedman98@gmail.com>
2022-06-20 21:57:23 -0400

    begin solution030

---
+++ js/solution030.js
@@ -0,0 +1,10 @@
+// take in the distance in miles, miles per gallon of the car, and gallons remaining
+// calculate if will reach the destination or not
+// P: number, number, number
+// R: true or false
+
+
+
+
+console.log(zeroFuel(50,25,2)) // true
+console.log(zeroFuel(60,25,2)) // false
