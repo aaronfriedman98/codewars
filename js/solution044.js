311500012de731c61259a5f7bdb0c176c4bd064a
Aaron Friedman <aronfriedman98@gmail.com>
2022-07-13 20:18:55 -0400

    solution044 completed

--- js/solution044.js
+++ js/solution044.js
@@ -2,17 +2,16 @@
 // P: 3 ints
 // R: 1 int
 // SOLUTION:    create an array to hold the sequence
-//              create a var to hold the solution
 //              create a loop that increments by the increment parameter and starts and ends with the parameters
 //              push each index into the array
 //              reduce the array into the sum of all its values and return it
 
 function sequenceSum(start, end, accum) {
     let array = []
-    for(let i = start; i <= end; i + accum) {
+    for(let i = start; i <= end; i += accum) {
         array.push(i)
     }
-    return array
+    return array.reduce((acc,i) => acc + i)
 }
 
 console.log(sequenceSum(1,10,2)) // 25
