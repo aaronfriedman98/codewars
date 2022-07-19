1438bf9f23878d25e01269f98f12d50e2a8fc1fb
Aaron Friedman <aronfriedman98@gmail.com>
2022-07-19 02:35:02 -0400

    solution049

---
+++ js/solution049.js
@@ -0,0 +1,12 @@
+// GOAL: take in an array, and return it minus the elements in the given array
+// P: array
+// R: array
+// SOLUTION:    loop through the array and check if each value matches the given arrays element and if not then delete the element with the indexOf method
+
+function gooseFilter (birds) {
+    var geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
+    
+   
+    }
+
+  console.log(gooseFilter(["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher", "hi"])) // ["hi"]
