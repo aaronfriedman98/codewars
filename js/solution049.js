e3d1d6415d5b7ee15f359b6205a7a37b393b1139
Aaron Friedman <aronfriedman98@gmail.com>
2022-07-19 03:28:20 -0400

    solution049 completed

--- js/solution049.js
+++ js/solution049.js
@@ -4,9 +4,15 @@
 // SOLUTION:    loop through the array and check if each value matches the given arrays element and if not then delete the element with the indexOf method
 
 function gooseFilter (birds) {
-    var geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
-    
-   
+    var geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"]
+    for(let i = 0; i < birds.length; i++) {
+        for(let j = 0; j < geese.length; j++) {
+            if(birds[i] == geese[j]){
+                birds.splice(i,1)
+            }
+        }
+    }
+    return birds
     }
 
-  console.log(gooseFilter(["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher", "hi"])) // ["hi"]
+  console.log(gooseFilter(["jack", "African", "Toulouse", "there", "Roman Tufted", "Steinbacher", "hi"])) // ["hi"]
