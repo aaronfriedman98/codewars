c912c5282e5c8653015fbb12fdf5ca5bd66b8312
Aaron Friedman <aronfriedman98@gmail.com>
2022-08-02 19:19:06 -0400

    solution056

---
+++ js/solution056.js
@@ -0,0 +1,12 @@
+// TASK: take in a string and remove all of the characters after the #
+// P: string
+// R: string
+// SOLUTION:    
+
+
+function removeUrlAnchor() {
+    
+}
+
+
+console.log(removeUrlAnchor("www.codewars.com#about")) // "www.codewars.com"
