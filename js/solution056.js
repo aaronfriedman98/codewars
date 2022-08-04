2e8bc82baea110e6ceffadf216b1b1b45ddbf604
Aaron Friedman <aronfriedman98@gmail.com>
2022-08-04 19:02:09 -0400

    solution056 completed

--- js/solution056.js
+++ js/solution056.js
@@ -1,13 +1,15 @@
-// TASK: given the total volume of a pyramid of cubes, determine the total number of cubes used in the pyramid
-// P: number, number (volume and number cubes)
-// R: number (number of cubes)
-// SOLUTION: 
+// TASK: take in the weight and height and find the body mass index. if <= 18.5 --> "underweight" if 25 --> "Normal" if 30 --> "Overweight" if > 30 --> "Obese"
+// P: int, int (weight, height)
+// R: string
+// SOLTUTION:   create var = weight/height**2
+//              check if bmi is less or equal to amounts
 
 
-
-function findNb(volume) {
-    
+function bmi(weight, height) {
+    const bmi = weight/height**2
+    if(bmi <= 18.5) return "Underweight"
+    else if(bmi <= 25) return "Normal"
+    else if(bmi <= 30) return "Overweight"
+    else return "Obese"
 }
 
-
-console.log(findNb(1071225)) // 45
