ff60ebc1674d1a01da74cb404510511d1b5cccff
Aaron Friedman <aronfriedman98@gmail.com>
2022-08-08 21:32:13 -0400

    solution057 completed

---
+++ js/solution057.js
@@ -0,0 +1,12 @@
+// TASK: take in a list of strings and return a new list only containing the strings which have exactly 4 letters
+// P: array of strings
+// R: array of strings
+// SOLUTION:    create a variable to hold the final array
+//              loop through the parameter array and check the length of each element
+//              if any have a length of 4 then push it to the final array
+
+function friend()
+
+
+
+console.log(friend(['aaron', 'jack', 'aron', 'nancy', 'mark'])) // ['jack','aron','mark']
