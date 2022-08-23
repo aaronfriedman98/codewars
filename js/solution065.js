dc0d27c167f21ed55f17ce59859ef80160d873e4
Aaron Friedman <aronfriedman98@gmail.com>
2022-08-22 20:34:49 -0400

    soluion065 completed

---
+++ js/solution065.js
@@ -0,0 +1,3 @@
+// TASK: pass in a number, and if more than 10 return "Great, now we move on to tricks.", if less then return "Keep at it until you get it."
+
+const hoopCount = (num) => num >= 10 ? "Great, now move on to tricks" : "Keep at it until you get it"
